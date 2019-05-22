5e97b2fd711d256fc56cdfb5b10296162f30a8b4
Dillon Kearns <dillon@dillonkearns.com>
2019-05-22 10:23:40 -0700

    Remove extra code.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,43 +1,24 @@
 module Main exposing (main)
 
-import Api.Query as Query
 import Browser
 import Browser.Navigation
-import Discount exposing (Discount)
 import Element exposing (Element)
 import Element.Border
 import Element.Events
 import Element.Input
-import Page.Home
-import Page.ProductDetail
-import Product exposing (Product)
+import Html.Attributes
 import RemoteData exposing (RemoteData)
-import Request exposing (Response)
-import Route exposing (Route)
 import Url exposing (Url)
-
-
-
--- Elm Architecture Setup
 
 
 type Msg
     = UrlRequested Browser.UrlRequest
     | UrlChanged Url
-    | HomeMsg Page.Home.Msg
-    | ProductDetailMsg Page.ProductDetail.Msg
 
 
 type alias Model =
-    { subModel : SubModel
-    , navKey : Browser.Navigation.Key
-    , route : Route
+    { navKey : Browser.Navigation.Key
     }
-
-
-type SubModel
-    = HomeModel Page.Home.Model
-    | ProductDetailModel Page.ProductDetail.Model
 
 
 type alias Flags =
@@ -46,17 +27,16 @@ type alias Flags =
 
 init : Flags -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
 init _ url navKey =
-    { subModel = Tuple.first Page.Home.init |> HomeModel
-    , navKey = navKey
-    , route = Route.Home
-    }
-        |> initPage url
+    ( { navKey = navKey
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case ( msg, model.subModel ) of
-        ( UrlRequested urlRequest, _ ) ->
+    case msg of
+        UrlRequested urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
                     ( model
@@ -66,37 +46,8 @@ update msg model =
                 Browser.External href ->
                     ( model, Browser.Navigation.load href )
 
-        ( UrlChanged url, _ ) ->
-            initPage url model
-
-        ( HomeMsg homeMsg, HomeModel subModel ) ->
-            Page.Home.update homeMsg subModel
-                |> updateWith HomeModel HomeMsg model
-
-        ( ProductDetailMsg subMsg, ProductDetailModel subModel ) ->
-            Page.ProductDetail.update subMsg subModel
-                |> updateWith ProductDetailModel ProductDetailMsg model
-
-        ( _, _ ) ->
+        UrlChanged url ->
             ( model, Cmd.none )
-
-
-initPage url model =
-    case Route.fromUrl url |> Maybe.withDefault Route.Home of
-        Route.Home ->
-            Page.Home.init
-                |> updateWith HomeModel HomeMsg model
-
-        Route.Product productId ->
-            Page.ProductDetail.init productId
-                |> updateWith ProductDetailModel ProductDetailMsg model
-
-
-updateWith : (subModel -> SubModel) -> (subMsg -> Msg) -> Model -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
-updateWith mapModel mapMsg model ( subModel, subCmd ) =
-    ( { model | subModel = mapModel subModel }
-    , Cmd.map mapMsg subCmd
-    )
 
 
 main : Program Flags Model Msg
@@ -115,15 +66,22 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "The Elm Shoppe"
     , body =
-        (case model.subModel of
-            HomeModel subModel ->
-                Page.Home.view subModel
-                    |> Element.map HomeMsg
-
-            ProductDetailModel subModel ->
-                Page.ProductDetail.view subModel
-                    |> Element.map ProductDetailMsg
-        )
+        [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+            |> List.map String.fromInt
+            |> List.map
+                (\n ->
+                    Element.paragraph [ Element.height (Element.px 100) ]
+                        [ Element.link []
+                            { url = "/#" ++ n
+                            , label =
+                                Element.text n
+                                    |> Element.el
+                                        [ Element.htmlAttribute (Html.Attributes.id n)
+                                        ]
+                            }
+                        ]
+                )
+            |> Element.column [ Element.centerX ]
             |> Element.layout []
             |> List.singleton
     }
